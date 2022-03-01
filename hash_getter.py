b6a87375cd72a4bd8a0a6562b51e8edb41404d11
Daniel Morala <morala84@gmail.com>
2022-03-01 21:32:44 +0100

    hash_getter

---
+++ hash_getter.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import hashlib
+
+def getsha256(file):
+    try:
+        hashsha = hashlib.sha256()
+        with open(file, "rb") as f:
+            for block in iter(lambda: f.read(4096), b""):
+                hashsha.update(block)
+        return hashsha.hexdigest()
+    except Exception as e:
+        print(f"Error: {e}")
+        return ""
+    except:
+        print("Error desconocido")
+        return ""
+
+pathfile = os.getcwd() + "/" + sys.argv[1]
+print(getsha256(pathfile))
